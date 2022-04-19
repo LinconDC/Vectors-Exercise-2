7e4e4512ad2026ebf437686c7999b2665413e966
linco <linco@DESKTOP-VIJS1C2>
2022-04-19 14:04:48 -0300

    new code in line 11

--- ExerciseVectors2/src/application/Program.java
+++ ExerciseVectors2/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.text.ParseException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,7 +8,7 @@ import entities.Rent;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
